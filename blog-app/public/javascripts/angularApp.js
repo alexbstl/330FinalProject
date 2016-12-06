38b652fc8b51b8d1a6519cd971379cd432a45720
Alex Bernstein <alexbstl@gmail.com>
2016-12-06 00:21:26 -0600

    small stuff

--- blog-app/public/javascripts/angularApp.js
+++ blog-app/public/javascripts/angularApp.js
@@ -24,11 +24,11 @@ app.factory('posts',['$http',
     function($http){
       var o = {
         posts:[
-          {title: 'post 1', upvotes: 5},
-          {title: 'post 2', upvotes: 2},
-          {title: 'post 3', upvotes: 15},
-          {title: 'post 4', upvotes: 9},
-          {title: 'post 5', upvotes: 4}
+          {title: 'post 1', upvotes: 5, comments: []},
+          {title: 'post 2', upvotes: 2, comments: []},
+          {title: 'post 3', upvotes: 15, comments: []},
+          {title: 'post 4', upvotes: 9, comments: []},
+          {title: 'post 5', upvotes: 4, comments: []}
         ]
       };
       o.getAll = function() {
