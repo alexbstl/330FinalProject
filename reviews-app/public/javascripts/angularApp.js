6a5a5c5cd4ab17209a8cc5cf7398674fff1a81b9
Alex Bernstein <alexbstl@gmail.com>
2016-12-07 01:38:01 -0600

    stuff

--- reviews-app/public/javascripts/angularApp.js
+++ reviews-app/public/javascripts/angularApp.js
@@ -9,70 +9,70 @@ app.config([
 
 
 
-.state('home', {
-  url: '/home',
-  templateUrl: '/home.html',
-  controller: 'MainCtrl',
-   resolve: {
-     postPromise: ['courses', function(courses){
-       return courses.getAll();
-     }]
-   }
-})
+    .state('home', {
+      url: '/home',
+      templateUrl: '/home.html',
+      controller: 'MainCtrl',
+      resolve: {
+        postPromise: ['courses', function(courses){
+          return courses.getAll();
+        }]
+      }
+    })
 
 
-.state('courses',{
-  url: '/courses/{id}',
-  templateUrl: '/courses.html',
-  controller: 'CoursesCtrl',
-   resolve: {
-     course: ['$stateParams', 'courses', function($stateParams, courses) {
-       return courses.get($stateParams.id);
-     }]
-   }
-});
-
-$urlRouterProvider.otherwise('home');
-}]);
-
-//COURSES FACTORY
-app.factory('courses', ['$http',
-function($http){
-
-  var crs = {courses: []}
-
-  //Create a New Course
-  crs.create = function(course) {
-    return $http.post('/courses', course).success(function(data){
-      crs.courses.push(data);
+    .state('courses',{
+      url: '/courses/{id}',
+      templateUrl: '/courses.html',
+      controller: 'CoursesCtrl',
+      resolve: {
+        course: ['$stateParams', 'courses', function($stateParams, courses) {
+          return courses.get($stateParams.id);
+        }]
+      }
     });
-  };
 
-  //Get All Courses
-  crs.getAll = function() {
-    return $http.get('/courses').success(function(data){
-      angular.copy(data, crs.courses);
-    });
-  };
+    $urlRouterProvider.otherwise('home');
+  }]);
 
-  //Get An Individual Course By ID
-   crs.get = function(id) {
-	 console.log("you're in here");
-	 return $http.get('/courses/' + id).then(function(res){
-		console.log(res);
-		return res.data;
-	 });
-   };
+  //COURSES FACTORY
+  app.factory('courses', ['$http',
+  function($http){
 
-   //Add a Review to a Course
- 	crs.addReview = function(id, review) {
-		return $http.post('/courses/' + id + '/reviews', review);
- 	};
-   
-   
-  return crs;
+    var crs = {courses: []}
 
-}
+    //Create a New Course
+    crs.create = function(course) {
+      return $http.post('/courses', course).success(function(data){
+        crs.courses.push(data);
+      });
+    };
+
+    //Get All Courses
+    crs.getAll = function() {
+      return $http.get('/courses').success(function(data){
+        angular.copy(data, crs.courses);
+      });
+    };
+
+    //Get An Individual Course By ID
+    crs.get = function(id) {
+      console.log("you're in here");
+      return $http.get('/courses/' + id).then(function(res){
+        console.log(res);
+        return res.data;
+      });
+    };
+
+    //Add a Review to a Course
+    crs.addReview = function(id, review) {
+      return $http.post('/courses/' + id + '/reviews', review);
+    };
+
+
+    return crs;
+
+  }
 ])
 
 app.controller('MainCtrl', [
@@ -94,7 +94,7 @@ app.controller('MainCtrl', [
       //If There Is No Link to Image, Populate With Default
       if(!$scope.img_link){
         $scope.img_link = "http://www.senseitout.com/wp-content/uploads/2016/01/3q.jpg";
-	  }
+      }
 
       //Log Input
       console.log("It Worked");
@@ -107,15 +107,15 @@ app.controller('MainCtrl', [
       //Create the New Course
       courses.create({
 
-		name: $scope.course_name,
-		code: $scope.course_code,
-		dept: $scope.depart,
-		prof: $scope.prof,
-		img_link : $scope.img_link,
-		overall_score : 0,
-		diff_score : 0,
-		teach_score : 0
-		
+        name: $scope.course_name,
+        code: $scope.course_code,
+        dept: $scope.depart,
+        prof: $scope.prof,
+        img_link : $scope.img_link,
+        overall_score : 0,
+        diff_score : 0,
+        teach_score : 0
+
       });
 
       //Clear Fields
@@ -123,7 +123,7 @@ app.controller('MainCtrl', [
       $scope.depart = "";
       $scope.course_code = "";
       $scope.prof = "";
-	  $scope.img_link = "";
+      $scope.img_link = "";
 
     }
   }
@@ -136,29 +136,33 @@ app.controller('CoursesCtrl', [
   function($scope,courses,course){
 
     $scope.course = course;
+    $scope.values=[1,2,3,4,5];
+    $scope.overall_score=5;
+    $scope.diff_score=5;
+    $scope.teach_score=5;
 
-	
-	
+
     //Add Review
     $scope.addReview = function() {
       if($scope.body ===''){return; }
 
-	  console.log("You're In");	
+      console.log("You're In");
       courses.addReview(course._id, {
-		  
+
         body: $scope.body,
-		overall_score : 0,
-		diff_score : 0,
-		teach_score : 0
-        
-		
+        overall_score : $scope.overall_score,
+        diff_score : $scope.diff_score,
+        teach_score : $scope.teach_score
+
+
       }).success(function(review) {
+        console.log(review);
         $scope.course.reviews.push(review);
       });
 
       $scope.body='';
     };
-	
-	
+
+
   }
 ]);
