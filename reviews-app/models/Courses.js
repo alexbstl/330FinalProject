5a76095502ef745ce0a70d13158e738ca5e1fe8f
Alex <alexbstl@gmail.com>
2017-05-03 00:15:57 -0500

    updates

--- reviews-app/models/Courses.js
+++ reviews-app/models/Courses.js
@@ -11,22 +11,20 @@ var CourseSchema = new mongoose.Schema({
   teach_score: Number,
   reviews: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Review' }]
 });
-CourseSchema.methods.totalScore = function(cb){
-  console.log(cb);
-  console.log("HERE");
-  overall_score = 0;
-  diff_score = 0;
-  teach_score=0;
-  console.log(this.reviews);
-  for (var i = 1;i<this.reviews.length;i++){
-    overall_score+=this.reviews[i].overall_score;
-    diff_score+=this.reviews[i].diff_score;
-    teach_score+=this.reviews[i].teach_score;
-  }
-  overall_score=overall_score/length(this.reviews)
-  diff_score=diff_score/length(this.reviews)
-  teach_score=teach_score/length(this.reviews)
-
-}
+//CourseSchema.methods.totalScore = function(cb){
+//  overall_score = 0;
+//  diff_score = 0;
+//  teach_score=0;
+//  console.log(this.reviews);
+//  for (var i = 1;i<this.reviews.length;i++){
+//    overall_score+=this.reviews[i].overall_score;
+//    diff_score+=this.reviews[i].diff_score;
+//    teach_score+=this.reviews[i].teach_score;
+//  }
+//  overall_score=overall_score/length(this.reviews)
+//  diff_score=diff_score/length(this.reviews)
+//  teach_score=teach_score/length(this.reviews)
+//
+//}
 
 mongoose.model('Course', CourseSchema);
